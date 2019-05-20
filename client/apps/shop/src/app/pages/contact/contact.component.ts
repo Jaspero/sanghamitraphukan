3a6049a2514f1f40d079d97ecefe9ace5d61f8d0
svendjanis <sven.djanis@gmail.com>
2019-05-20 13:58:30 +0200

    feat(contact): contact form closes #11

--- client/apps/shop/src/app/pages/contact/contact.component.ts
+++ client/apps/shop/src/app/pages/contact/contact.component.ts
@@ -1,4 +1,10 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FirestoreCollections} from '@jf/enums/firestore-collections.enum';
+import {notify} from '@jf/utils/notify.operator';
+import * as nanoid from 'nanoid';
+import {from} from 'rxjs';
 
 @Component({
   selector: 'jfs-contact',
@@ -7,7 +13,34 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ContactComponent implements OnInit {
-  constructor() {}
+  constructor(private fb: FormBuilder, private afs: AngularFirestore) {}
 
-  ngOnInit() {}
+  form: FormGroup;
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  send() {
+    const formData = this.form.getRawValue();
+
+    from(
+      this.afs
+        .collection(FirestoreCollections.Contacts)
+        .doc(nanoid())
+        .set(formData)
+    )
+      .pipe(notify())
+      .subscribe(() => {
+        this.form.reset();
+      });
+  }
 }
