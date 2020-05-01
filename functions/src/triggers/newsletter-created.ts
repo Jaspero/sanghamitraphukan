f15b379741a7b7cdeb490a504ab742e9e0b743d6
Filip Lauc <filip.lauc93@gmail.com>
2020-05-01 13:09:54 +0200

    chore: minor updates

--- functions/src/triggers/newsletter-created.ts
+++ functions/src/triggers/newsletter-created.ts
@@ -1,12 +1,12 @@
 import * as functions from 'firebase-functions';
-import * as rp from 'request-promise-native';
-import {ENV_CONFIG} from '../consts/env-config.const';
 import {parseEmail} from '../utils/parse-email';
 
 export const newsletterCreated = functions.firestore
   .document('newsletter/{id}')
   .onCreate(async snap => {
     const data: any = snap.data();
+
+    console.log(data);
 
     if (data.discount) {
       const sent = await parseEmail(
@@ -22,22 +22,6 @@ export const newsletterCreated = functions.firestore
         email: snap.id
       });
     }
-
-    try {
-      await rp({
-        method: 'POST',
-        uri: `https://us20.api.mailchimp.com/3.0/lists/${ENV_CONFIG.mailchimp.list}/members/`,
-        auth: {
-          user: 'username',
-          pass: ENV_CONFIG.mailchimp.token
-        },
-        body: {
-          email_address: snap.id,
-          status: 'subscribed'
-        },
-        json: true
-      });
-    } catch (e) {}
 
     return true;
   });
