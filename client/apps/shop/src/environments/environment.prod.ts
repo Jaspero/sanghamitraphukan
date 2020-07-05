db8fc379a8939701b39ea014549cc2a2c81f8c1e
AntonioStipic <antonio.stipic2@gmail.com>
2020-07-05 22:21:04 +0200

    fix: environment.prod.ts #26

--- client/apps/shop/src/environments/environment.prod.ts
+++ client/apps/shop/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   serviceWorker: true,
-  websiteUrl: 'https://sanghamitrapukan.com',
+  websiteUrl: 'https://sanghamitraphukan.com',
   restApi: 'https://us-central1-jaspero-sanghamitra.cloudfunctions.net',
   production: true
 };
