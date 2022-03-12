36018608872b6c52c569d65bb29c539bfb0b0ff2
koleCar <h.kolaric92@gmail.com>
2022-03-12 07:50:49 +0100

    #7

--- client/apps/shop/src/app/shared/services/currency/currency-rates.service.ts
+++ client/apps/shop/src/app/shared/services/currency/currency-rates.service.ts
@@ -1,58 +1,84 @@
+import { HttpClient } from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {AngularFireFunctions} from '@angular/fire/functions';
+import { ActivatedRoute } from '@angular/router';
 import {DYNAMIC_CONFIG} from '@jf/consts/dynamic-config.const';
-import {BehaviorSubject, from} from 'rxjs';
+import { BehaviorSubject, from, Subject } from 'rxjs';
 import {filter, map, switchMap, take} from 'rxjs/operators';
+import { COUNTRY_CURRENCY } from '../../consts/country-currency.const';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyRatesService {
-  constructor(private aff: AngularFireFunctions) {}
+  constructor(
+    private aff: AngularFireFunctions,
+    private http: HttpClient,
+    private route: ActivatedRoute
+  ) {}
 
   static COUNTRY_CACHE_KEY = 'country-req';
   static CURRENT_CODE = 'current-code';
 
   current$: BehaviorSubject<string>;
 
-  getRates() {
-    let currentCode: any = localStorage.getItem(
-      CurrencyRatesService.CURRENT_CODE
-    );
-    let countryCheck = localStorage.getItem(
-      CurrencyRatesService.COUNTRY_CACHE_KEY
-    );
+  missingCurrency() {
+    from(this.aff.functions.httpsCallable('ipData')())
+      .pipe(
+        filter(value => !!value),
+        switchMap(value =>
+          this.current$.pipe(map(current => [current, value.data]))
+        ),
+        take(1)
+      )
+      .subscribe(([current, countryCurrency]) => {
+        localStorage.setItem(CurrencyRatesService.COUNTRY_CACHE_KEY, 'sent');
 
-    this.current$ = new BehaviorSubject(
-      currentCode ? currentCode : DYNAMIC_CONFIG.currency.primary
-    );
+        if (
+          countryCurrency !== current &&
+          DYNAMIC_CONFIG.currency.supportedCurrencies.includes(
+            countryCurrency
+          )
+        ) {
+          this.current$.next(countryCurrency);
+        }
+      });
+  }
+
+  getRates() {
+    this.current$ = new BehaviorSubject( DYNAMIC_CONFIG.currency.primary);
+    this.route.queryParams.subscribe(async (val) => {
+      let currentCode
+      let countryCheck = localStorage.getItem(
+        CurrencyRatesService.COUNTRY_CACHE_KEY
+      );
+      if (val.currency && DYNAMIC_CONFIG.currency.supportedCurrencies.includes(val.currency)) {
+        currentCode = val.currency;
+      } else {
+        currentCode = localStorage.getItem(
+          CurrencyRatesService.CURRENT_CODE
+        );
+      }
+      if (!currentCode) {
+        const ipLocation: any = await this.http.get('https://api.codetabs.com/v1/geolocation/json').toPromise()
+        const found = COUNTRY_CURRENCY.find(x => x.countries.includes(ipLocation.country_code))
+        if (found && found.currency) {
+          currentCode = found.currency
+        }
+      }
+
+      if (!currentCode && !countryCheck) {
+        this.missingCurrency()
+      }
+    });
+
+
 
     this.current$.subscribe(change => {
       localStorage.setItem(CurrencyRatesService.CURRENT_CODE, change);
       DYNAMIC_CONFIG.currency.primary = change;
     });
 
-    if (!currentCode && !countryCheck) {
-      from(this.aff.functions.httpsCallable('ipData')())
-        .pipe(
-          filter(value => !!value),
-          switchMap(value =>
-            this.current$.pipe(map(current => [current, value.data]))
-          ),
-          take(1)
-        )
-        .subscribe(([current, countryCurrency]) => {
-          localStorage.setItem(CurrencyRatesService.COUNTRY_CACHE_KEY, 'sent');
 
-          if (
-            countryCurrency !== current &&
-            DYNAMIC_CONFIG.currency.supportedCurrencies.includes(
-              countryCurrency
-            )
-          ) {
-            this.current$.next(countryCurrency);
-          }
-        });
-    }
   }
 }
